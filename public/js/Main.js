17e7eb990d0961066ca57993020e6edc2c72d9f2
Nicholas J. Olson <nicholasjamesolson@gmail.com>
2016-07-29 13:45:51 -0500

    create circle graphic outside of for loop

--- public/js/Main.js
+++ public/js/Main.js
@@ -5,9 +5,10 @@ var game = new Phaser.Game(800, 800, Phaser.auto, 'game-space', {
 function preload() {}
 
 function create() {
+	var circle = game.add.graphics(0, 0);
+	circle.beginFill(0xFF0000, 1);
 	for(var i = 0; i < 4; i++) {
-	  var circle = game.add.graphics(0, 0);
-	  circle.beginFill(0xFF0000, 1);
+		var circle_sprite = game.add.sprite(200, 200, circle)
 	  var center = game.width/2;
 	  console.log(center)
 	  if (i == 0 ) {
@@ -19,17 +20,29 @@ function create() {
 	  } else {
 	  	circle.drawCircle(center, center - 100, 100)
 	  }
-
+	  // trying to get the click to work!!!!!!
+	  // circle.events.onInputDown.add(listener, circle);
+		// circle.downPoint()
+		circle_sprite.input.event.downPoint(spit(), this)
 	}
 }
 
+function spit() {
+		console.log('it worked')
+}
 function moveCircle(pointer) {
-	circleX = circle.x
-	circleY = circle.y
-	this.x += 20;
+	// circleX = circle.x
+	// circleY = circle.y
+	// this.x += 20;
 	// if (circleX != game.width / 2 - 10 && )
 }
+function listener () {
 
+    // counter++;
+    // text.text = "You clicked " + counter + " times!";
+    console.log(this)
+
+}
 // Add a function to find position along circle circumferances line
 
 function update() {}
